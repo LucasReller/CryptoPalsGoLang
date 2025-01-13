1716b735e8b8a672ef33df20c336cda3bdd4e234
LReller <lucas.reller@comcast.net>
2025-01-12 21:33:58 -0600

    Complete challenge two

--- Set_1/Challenge 2/main.go
+++ Set_1/Challenge 2/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	hexStringOne := []byte("1c0111001f010100061a024b53535009181c")
+	hexStringTwo := []byte("686974207468652062756c6c277320657965")
+
+	hexResultOne := decodeHex(hexStringOne)
+	//fmt.Println(hexResultOne)
+
+	hexResultTwo := decodeHex(hexStringTwo)
+	//fmt.Println(hexResultTwo)
+
+	result := xor(hexResultOne, hexResultTwo)
+	fmt.Printf("%s", encodeHex(result))
+}
+
+func decodeHex(src []byte) []byte {
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	hex.Decode(dst, src)
+	return dst
+}
+
+func encodeHex(src []byte) []byte {
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dst, src)
+	return dst
+}
+
+func xor(first, second []byte) []byte {
+	resultBytes := make([]byte, len(first))
+
+	for i := 0; i < len(first); i++ {
+		resultBytes[i] = first[i] ^ second[i]
+	}
+	return resultBytes
 }
