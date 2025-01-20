359b7985ff94f03ef4b00987ef1914a1416904eb
LReller <lucas.reller@comcast.net>
2025-01-20 08:50:13 -0600

    Challenge 6 slight cleanup

--- Set_1/Challenge6/main.go
+++ Set_1/Challenge6/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
@@ -12,9 +11,6 @@ import (
 
 const keySizeMin int = 2
 const keySizeMax int = 40
-const chars string = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-const vowels string = "aeiouy" //these 6 letters account for 39.8% of the characters found in english sentences
-const targetPercent float64 = .398
 
 func main() {
 	fmt.Println(computeEditDistance([]byte("this is a test"), []byte("wokka wokka!!!")))
@@ -51,55 +47,6 @@ func findKey(arr [][]byte) string {
 	return result
 }
 
-var letterFrequencies = map[string]float64{
-	"A": 8.167,
-	"B": 1.492,
-	"C": 2.782,
-	"D": 4.253,
-	"E": 12.702,
-	"F": 2.228,
-	"G": 2.015,
-	"H": 6.094,
-	"I": 6.966,
-	"J": 0.153,
-	"K": 0.772,
-	"L": 4.025,
-	"M": 2.406,
-	"N": 6.749,
-	"O": 7.507,
-	"P": 1.929,
-	"Q": 0.095,
-	"R": 5.987,
-	"S": 6.327,
-	"T": 9.056,
-	"U": 2.758,
-	"V": 0.978,
-	"W": 2.360,
-	"X": 0.150,
-	"Y": 1.974,
-	"Z": 0.074,
-}
-
-var text = regexp.MustCompile("^[a-zA-Z ]$")
-
-func isAlphabetic(str string) bool {
-	return text.MatchString(str)
-}
-
-func calculateScore(buffer []byte) float64 {
-	score := 0.0
-	for _, b := range buffer {
-		str := string(b)
-		if isAlphabetic(str) {
-			score += letterFrequencies[strings.ToUpper(str)]
-		} else {
-			score -= 10.0
-		}
-	}
-
-	return score
-}
-
 func singleByteXOR(buffer []byte, key byte) []byte {
 	result := make([]byte, len(buffer))
 	for i := range len(buffer) {
@@ -107,23 +54,6 @@ func singleByteXOR(buffer []byte, key byte) []byte {
 	}
 
 	return result
-}
-
-func scoreEachLetter(encodedValue []byte) (byte, float64) {
-	bestKey := byte(0)
-	bestScore := 0.0
-	for k := range 256 {
-		key := byte(k)
-		decrypted := singleByteXOR(encodedValue, key)
-
-		score := calculateScore(decrypted)
-
-		if score > bestScore {
-			bestScore = score
-			bestKey = key
-		}
-	}
-	return bestKey, bestScore
 }
 
 func createBlocks(file []byte, keysize int) [][]byte {
@@ -189,14 +119,72 @@ func computeEditDistance(value1, value2 []byte) int {
 	return int(result)
 }
 
-func encodeHex(src []byte) []byte {
-	dst := make([]byte, hex.EncodedLen(len(src)))
-	hex.Encode(dst, src)
-	return dst
+//region english scoring code
+
+var letterFrequencies = map[string]float64{
+	"A": 8.167,
+	"B": 1.492,
+	"C": 2.782,
+	"D": 4.253,
+	"E": 12.702,
+	"F": 2.228,
+	"G": 2.015,
+	"H": 6.094,
+	"I": 6.966,
+	"J": 0.153,
+	"K": 0.772,
+	"L": 4.025,
+	"M": 2.406,
+	"N": 6.749,
+	"O": 7.507,
+	"P": 1.929,
+	"Q": 0.095,
+	"R": 5.987,
+	"S": 6.327,
+	"T": 9.056,
+	"U": 2.758,
+	"V": 0.978,
+	"W": 2.360,
+	"X": 0.150,
+	"Y": 1.974,
+	"Z": 0.074,
 }
 
-func decodeHex(src []byte) []byte {
-	dst := make([]byte, hex.DecodedLen(len(src)))
-	hex.Decode(dst, src)
-	return dst
+var text = regexp.MustCompile("^[a-zA-Z ]$")
+
+func isAlphabetic(str string) bool {
+	return text.MatchString(str)
 }
+
+func calculateScore(buffer []byte) float64 {
+	score := 0.0
+	for _, b := range buffer {
+		str := string(b)
+		if isAlphabetic(str) {
+			score += letterFrequencies[strings.ToUpper(str)]
+		} else {
+			score -= 10.0
+		}
+	}
+
+	return score
+}
+
+func scoreEachLetter(encodedValue []byte) (byte, float64) {
+	bestKey := byte(0)
+	bestScore := 0.0
+	for k := range 256 {
+		key := byte(k)
+		decrypted := singleByteXOR(encodedValue, key)
+
+		score := calculateScore(decrypted)
+
+		if score > bestScore {
+			bestScore = score
+			bestKey = key
+		}
+	}
+	return bestKey, bestScore
+}
+
+//end region english scoring code
